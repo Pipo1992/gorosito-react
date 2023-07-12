5ca27916c2fdf08759cec5892fd6a6afaff6995e
Pablo Gorosito <pablo.92pgg@gmail.com>
2023-07-12 20:48:47 +0200

    Se mejora la visión del proyecto

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,29 +1,30 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Item from "./Item";
+import "./Card.css";
 
 const ItemList = () => {
   const { name } = useParams();
- 
+
   const [listaProductos, setListaProductos] = useState([]);
   useEffect(() => {
-      const urlToFetch = name 
-      ? `https://fakestoreapi.com/products/category/${name}` 
+    const urlToFetch = name
+      ? `https://fakestoreapi.com/products/category/${name}`
       : "https://fakestoreapi.com/products"
-      const allProducts = async () => {
-        try {
-          const response = await fetch(urlToFetch);
-          const data = await response.json();
-          setListaProductos(data);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-      allProducts()
+    const allProducts = async () => {
+      try {
+        const response = await fetch(urlToFetch);
+        const data = await response.json();
+        setListaProductos(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    allProducts()
   }, [name]);
 
   return (
-    <div>
+    <div className="item-list-container">
       {listaProductos &&
         listaProductos.map((producto, i) => (
           <Item key={i} producto={producto} />
